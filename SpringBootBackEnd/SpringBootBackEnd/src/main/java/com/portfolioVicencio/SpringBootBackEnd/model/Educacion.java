b01c47bc1ee50863c7f9716cc3b708ce1ba71a35
mosketon <94483901+mosketon@users.noreply.github.com>
2022-05-12 01:43:23 -0300

    Conexion DB
    Creada DB, configurada conexion, generadas tablas correctamente

--- SpringBootBackEnd/SpringBootBackEnd/src/main/java/com/portfolioVicencio/SpringBootBackEnd/model/Educacion.java
+++ SpringBootBackEnd/SpringBootBackEnd/src/main/java/com/portfolioVicencio/SpringBootBackEnd/model/Educacion.java
@@ -1,13 +1,23 @@
 package com.portfolioVicencio.SpringBootBackEnd.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+
+@Entity
+
 public class Educacion {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
     private String establecimiento;
     private String desde;
     private String hasta;
--- SpringBootBackEnd/SpringBootBackEnd/src/main/java/com/portfolioVicencio/SpringBootBackEnd/model/Habilidades.java
+++ SpringBootBackEnd/SpringBootBackEnd/src/main/java/com/portfolioVicencio/SpringBootBackEnd/model/Habilidades.java
@@ -1,13 +1,22 @@
 package com.portfolioVicencio.SpringBootBackEnd.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 
+@Entity
+
 public class Habilidades {
     
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
+    
     private String tipo;
     private String porcentaje;
     

dc82946fca2ddd587a00c8c8306c53ce87cbd9e3
mosketon <94483901+mosketon@users.noreply.github.com>
2022-05-13 15:15:15 -0300

    Controladores
    Funcionando con endpoints

--- SpringBootBackEnd/SpringBootBackEnd/src/main/java/com/portfolioVicencio/SpringBootBackEnd/controller/ProyectosController.java
+++ SpringBootBackEnd/SpringBootBackEnd/src/main/java/com/portfolioVicencio/SpringBootBackEnd/controller/ProyectosController.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.portfolioVicencio.SpringBootBackEnd.controller;
 
-/**
- *
- * @author kaise
- */
+import com.portfolioVicencio.SpringBootBackEnd.model.Proyectos;
+import com.portfolioVicencio.SpringBootBackEnd.service.IProyectosService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProyectosController {
+    
+    @Autowired
+     private IProyectosService proServ;
+    
+    @PostMapping ("/new/proyectos")
+    public void agregarProyectos (@RequestBody Proyectos pro){
+        proServ.crearProyectos(pro);
+    }
+    
+    @GetMapping ("/ver/proyectos")
+    @ResponseBody
+    public List<Proyectos> verProyectos(){
+        return proServ.verProyectos();
+    }
+    
+    @DeleteMapping ("/delete/proyectos/{id}")
+    public void borrarProyectos (@PathVariable Long id){
+        proServ.borrarProyectos(id);
+    }
     
 }

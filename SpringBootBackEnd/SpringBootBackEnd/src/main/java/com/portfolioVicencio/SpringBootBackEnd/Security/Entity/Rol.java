45bf67a791095ab129e34fe2e2095e3f75966ce6
mosketon <94483901+mosketon@users.noreply.github.com>
2022-07-06 17:58:15 -0300

    Paquete Seguridad
    Creado los paquetes entity y enum
    Calases Rol, usuario y usuario principal

---
+++ SpringBootBackEnd/SpringBootBackEnd/src/main/java/com/portfolioVicencio/SpringBootBackEnd/Security/Entity/Rol.java
@@ -0,0 +1,48 @@
+package com.portfolioVicencio.SpringBootBackEnd.Security.Entity;
+
+import com.portfolioVicencio.SpringBootBackEnd.Security.Enums.RolNombre;
+import com.sun.istack.NotNull;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+    
+    //Constructor
+
+    public Rol() {
+    }
+
+    public Rol(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+    
+    //Getter y Setter
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+    
+}

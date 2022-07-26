4e62f184486d53846a4a85a86da144e63c390f1b
mosketon <94483901+mosketon@users.noreply.github.com>
2022-07-26 11:16:32 -0300

    version 1.0.1

--- SpringBootBackEnd/SpringBootBackEnd/src/main/java/com/portfolioVicencio/SpringBootBackEnd/Security/Controller/AuthController.java
+++ SpringBootBackEnd/SpringBootBackEnd/src/main/java/com/portfolioVicencio/SpringBootBackEnd/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin (origins = "http://localHost:4200")
+@CrossOrigin (origins = "https://portfoliovicencioap.web.app")
 
 public class AuthController {
 
